c40ab86fbd0956eb53cae1cc636e82d3eb8d6dc2
v1tal4ik <vetalgta@gmail.com>
2019-02-18 16:34:11 +0200

    v.02.1.1

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-cdwhrjbcfeivbfisubviufdvbibvifvnifudbvifdbvidbu
+var http = require('http');
+
+http.createServer(function(res,req){
+    console.log('HTTP server running');
+    res.writeHead(200,{
+        'Content-Type':'text/html'});
+}).listen(3000);
