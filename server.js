d60f2fe092d1ac2ad7c18e813301559a1408156b
v1tal4ik <vetalgta@gmail.com>
2019-02-18 20:31:47 +0200

    v.02.2.1

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var http = require('http');
 
-http.createServer(function(res,req){
-    console.log('HTTP server running');
-    res.writeHead(200,{
-        'Content-Type':'text/html'});
+const server = http.createServer(()=>{
+    console.log('http server running');
 }).listen(3000);
+
+server.on('request',(req,res)=>{
+    const consoleTime = setInterval(()=>{console.log(getTime());},2000);
+    const resTime = setTimeout(()=>{
+        res.end(`${getTime()}`);
+        clearInterval(consoleTime);
+    },10000);
+});
+
+
+
+function getTime(){
+    let date = new Date(),
+        hours = date.getHours(),
+        minutes = date.getMinutes(),
+        second = date.getSeconds(),
+        day = date.getDate(), 
+        month = date.getMonth(), 
+        year = date.getFullYear(),
+        dateNow = `${hours}:${minutes}:${second}  ${day}.0${month}.${year}`; 
+    return dateNow;
+}
