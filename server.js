0395cd95709fec919b9a826a3ff7f1d550992b4c
v1tal4ik <vetalgta@gmail.com>
2019-02-18 19:57:40 +0200

    v.02.2.2

--- server.js
+++ server.js
@@ -1,43 +1,108 @@
 const fs = require('fs');
 const path = require('path');
+const yargs = require('yargs');
+const del = require('del');
+
+const argv = yargs
+    .usage('Usage $0 [options]')
+    .help('help')
+    .alias('help', 'h')
+    .version('0.0.1')
+    .alias('version', 'v')
+    .example('$0 --entry ./filesDir -d')
+    .option('entry',{
+        alias:'e',
+        describe:'The path of source directory',
+        demandOption: true
+    })
+    .option('output',{
+        alias:'o',
+        describe:'The path of output directory',
+        default: '/dest'
+    })
+    .option('delete',{
+        alias: 'd',
+        describe:'Delete source directiory',
+        default: false
+    })
+    .argv
+
+    console.log('copying...');
 
 
-const src = path.join(process.argv[1],process.argv[2]);
-const dest = path.join(process.argv[1],process.argv[3]);
-const deleting = process.argv[4];
+
+
+const src = path.normalize(path.join(__dirname,argv.entry));
+const dest = path.normalize(path.join(__dirname,argv.output));
+const deleting = argv.delete;
+
+const promises = [];
 
 
 
-const readDir=(base,dest,deleting,level)=>{
-    try{
-        if(!fs.existsSync(dest)){
-            fs.mkdirSync(dest);
-        }
 
-
-        const files = fs.readdirSync(base);
-
-        files.forEach(item => {
-            let localBase = path.join(base,item);
-            let state = fs.statSync(localBase);
-            
-            if(state.isDirectory()){
-                readDir(localBase,dest,deleting,level+1);
-            }else{
-                let letter = item.toString().split('')[0];
-
-                if(!fs.existsSync(`${dest}/${letter}`)){
-                    fs.mkdirSync(`${dest}/${letter}`);
-                };
-
-                fs.copyFileSync(localBase,`${dest}/${letter}/${item}`);
-            }
-        });
-
-    }catch(error){
-        console.log(error);
-    }
+const readDir=(base)=>{
+    return new Promise((resolve,reject)=>{
+        fs.readdir(base,(err,data)=>{
+            if(err){reject(err)};
+            resolve(data);
+        })
+    })
 }
-readDir(src,dest,deleting,0);
+
+const isFolder = async currentURL=>{
+    return new Promise((resolve,reject)=>{
+    
+        fs.stat(currentURL,(err,stat)=>{
+            if(err){reject(err)};
+            resolve(stat.isDirectory());
+        });
+    })
+};
+
+const createDir = path =>{
+   if(!fs.existsSync(path)){
+       fs.mkdirSync(path);
+   }
+}
+
+const copyFiles = async src=>{
+    const files = await readDir(src);
+    
+    for(let i=0; i < files.length; i++){
+        let localBase = path.join(src,files[i]);
+        let isDir = await isFolder(localBase);
+
+        if(isDir){
+            await copyFiles(localBase);
+        }else{
+            promises.push(new Promise((resolve,reject)=>{
+                let newSubFolder = path.join(dest,files[i].toString().split('')[0]);
+                createDir(dest);
+                createDir(newSubFolder);
+                fs.copyFileSync(localBase,`${newSubFolder}/${files[i]}`);
+                fs.unlink(localBase,()=>{
+                    resolve();
+                });
+            }));
+        }
+    }
+} 
+
+const searchFiles = async src =>{
+    await copyFiles(src);
+
+    return Promise.all(promises);
+}
+
+searchFiles(src)
+    .then(()=>{
+        console.log('********************* Copying finished *******************');
+        if(deleting==='true'){
+            del(`${path.join(src, path.sep)}**`);
+            console.log(`Source dir was DELETE! `);
+        }
+    });
+
 
    
